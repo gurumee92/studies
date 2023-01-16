6ff12c60c06a42a4192129a7e3314e7cb6de8fd3
장현우 <gurumee92@gmail.com>
2023-01-16 22:16:53 +0900

    [nc-getting-started-dart/ch02] code: Update string interpolation examples

--- nc-getting-started-dart/ch02/main.dart
+++ nc-getting-started-dart/ch02/main.dart
@@ -1,11 +1,18 @@
 void main(List<String> args) {
+  // List
   var giveMeFive = false;
   var numbers = [
     1,
     2,
     3,
     4,
-    if (giveMeFive) 5,
+    if (giveMeFive) 5, // for if
   ];
   print(numbers);
+
+  // string interpolation
+  var name = "nico";
+  var age = 10;
+  var greeting = "Hello everyone, my name is ${name} my age is ${age + 2}";
+  print(greeting);
 }
