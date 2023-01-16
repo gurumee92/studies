03d77e78412795118087d87c1b2d899e0adf7187
장현우 <gurumee92@gmail.com>
2023-01-16 22:05:31 +0900

    [nc-getting-started-dart/ch02] code: Add basic data types

---
+++ nc-getting-started-dart/ch02/main.dart
@@ -0,0 +1,10 @@
+void main(List<String> args) {
+  String name = "nico";
+  bool alive = true;
+  int age = 12;
+  double money = 12.0;
+  print(name);
+  print(alive);
+  print(age);
+  print(money);
+}
