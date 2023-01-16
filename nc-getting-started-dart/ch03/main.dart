2b3c8b11d68e1fcb191d1b68ca4b98a50e78a15d
장현우 <gurumee92@gmail.com>
2023-01-16 22:46:05 +0900

    [nc-getting-started-dart/ch03] code: Update named parameters example

--- nc-getting-started-dart/ch03/main.dart
+++ nc-getting-started-dart/ch03/main.dart
@@ -1,6 +1,12 @@
-String sayHello(String name) => "hello $name, nice to meet you";
+// named parameter default
+String sayHello({String name = "nico"}) => "hello $name, nice to meet you";
+
+// named parameter required
+num add({required num a, b}) => a + b;
 
 void main() {
-  var say = sayHello("nico");
+  var say = sayHello(name: "gurumee");
   print(say);
+  var result = add(a: 3, b: 5);
+  print(result);
 }
