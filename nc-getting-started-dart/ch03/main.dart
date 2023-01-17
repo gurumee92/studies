603a4427e4df2efb5c458ffb4d0109e800286127
장현우 <gurumee92@gmail.com>
2023-01-17 21:37:00 +0900

    [nc-getting-started-dart/ch03] code: Update add qq operator  examples

--- nc-getting-started-dart/ch03/main.dart
+++ nc-getting-started-dart/ch03/main.dart
@@ -8,6 +8,9 @@ num add({required num a, b}) => a + b;
 String sayNameContury(String name, [String? contury = "Korean"]) =>
     "my name is $name and I'm from $contury";
 
+// qq operator
+String capitalizeName(String? name) => name?.toUpperCase() ?? "None";
+
 void main() {
   var say = sayHello(name: "gurumee");
   print(say);
@@ -16,4 +19,7 @@ void main() {
   say = sayNameContury("nico");
   print(say);
   print(sayNameContury("gurumee", "American"));
+
+  print(capitalizeName("gurumee"));
+  print(capitalizeName(null));
 }
