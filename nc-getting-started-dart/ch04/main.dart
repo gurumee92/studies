e2b4c131ef9b9ae42681a74ba6db1c078c08d116
장현우 <gurumee92@gmail.com>
2023-01-18 22:55:02 +0900

    [nc-getting-started-dart/ch04] code: Update cascade notation example

--- nc-getting-started-dart/ch04/main.dart
+++ nc-getting-started-dart/ch04/main.dart
@@ -1,6 +1,6 @@
 class Player {
-  late final String name;
-  late final int xp;
+  String name;
+  int xp;
 
   Player({required this.name, required this.xp});
   Player.create1000XpPlayer({required String name})
@@ -13,9 +13,10 @@ class Player {
 }
 
 void main(List<String> args) {
-  var p1 = Player(name: "gurumee", xp: 1500);
+  var p1 = Player(name: "gurumee", xp: 1500)
+    ..name = "nico"
+    ..xp = 50;
   p1.sayHello();
-
   var p2 = Player.create1000XpPlayer(name: "nico");
   p2.sayHello();
 }
