68a5f552b5bf68a3561d62dba7444c74e4d91949
장현우 <gurumee92@gmail.com>
2023-01-25 20:42:13 +0900

    [nc-getting-started-flutter/ui-challenge] code: Update UI CHALLENGE
    1. update Header

--- nc-getting-started-flutter/wallet/lib/main.dart
+++ nc-getting-started-flutter/wallet/lib/main.dart
@@ -1,27 +1,52 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(App());
+  runApp(const App());
 }
 
 class App extends StatelessWidget {
+  const App({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Wallet',
       home: Scaffold(
-          backgroundColor: Colors.red,
-          body: Column(
-            children: [
-              Text(
-                "Hi Selena,",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 18,
-                  fontWeight: FontWeight.w600,
+          backgroundColor: const Color(0xFF181818),
+          body: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 40),
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 80,
                 ),
-              ),
-            ],
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        const Text(
+                          "Hey, Selena",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 38,
+                            fontWeight: FontWeight.w800,
+                          ),
+                        ),
+                        Text(
+                          "Welcome back",
+                          style: TextStyle(
+                            color: Colors.white.withOpacity(0.7),
+                            fontSize: 18,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
           )),
     );
   }
