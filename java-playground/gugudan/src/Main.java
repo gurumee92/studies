58403df2107c1c8e72e1988fc950d3fcb4d8a405
gurumee92 <gurumee92@woowahan.com>
2023-07-24 07:02:09 +0900

    code: playground #2 gugudan; 2단 구현

--- java-playground/gugudan/src/Main.java
+++ java-playground/gugudan/src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // 2단
+        System.out.printf("%d x %d = %d\n", 2, 1, 2 * 1);
+        System.out.printf("%d x %d = %d\n", 2, 2, 2 * 2);
+        System.out.printf("%d x %d = %d\n", 2, 3, 2 * 3);
+        System.out.printf("%d x %d = %d\n", 2, 1, 2 * 4);
+        System.out.printf("%d x %d = %d\n", 2, 2, 2 * 5);
+        System.out.printf("%d x %d = %d\n", 2, 3, 2 * 6);
+        System.out.printf("%d x %d = %d\n", 2, 1, 2 * 7);
+        System.out.printf("%d x %d = %d\n", 2, 2, 2 * 8);
+        System.out.printf("%d x %d = %d\n", 2, 3, 2 * 9);
+
+        // 3단
+        System.out.printf("%d x %d = %d\n", 2, 1, 3 * 1);
+        System.out.printf("%d x %d = %d\n", 2, 2, 3 * 2);
+        System.out.printf("%d x %d = %d\n", 2, 3, 3 * 3);
+        System.out.printf("%d x %d = %d\n", 2, 1, 3 * 4);
+        System.out.printf("%d x %d = %d\n", 2, 2, 3 * 5);
+        System.out.printf("%d x %d = %d\n", 2, 3, 3 * 6);
+        System.out.printf("%d x %d = %d\n", 2, 1, 3 * 7);
+        System.out.printf("%d x %d = %d\n", 2, 2, 3 * 8);
+        System.out.printf("%d x %d = %d\n", 2, 3, 3 * 9);
     }
 }
