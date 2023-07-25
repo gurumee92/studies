346b3761f1c3222c6ca6710f9497e3b8e8632ac2
gurumee92 <gurumee92@woowahan.com>
2023-07-25 14:59:46 +0900

    code: playground #2 gugudan; 메소드로 구구단 구현하기

--- java-playground/gugudan/README.md
+++ java-playground/gugudan/README.md
@@ -113,3 +113,30 @@ public class Gugudan {
     }
 }
 ```
+
+### 힌트 6.
+```java
+public class Gugudan {
+    public static int[] calculate(int times) {
+        int[] result = new int[9];
+
+        for (int i = 0; i < result.length; i++) {
+            // 계산 결과를 배열에 저장
+        }
+
+        return result;
+    }
+
+    public static void print(int[] result) {
+        for (int i = 0; i < result.length; i++) {
+            // 계산 결과를 화면에 출력
+        }
+    }
+
+    public static void main(String[] args) {
+        for (int j = 2; j < 10; j++) {
+            // calculate와 print 메소드 활용해 구구단 구현하기
+        }
+    }
+}
+```
--- java-playground/gugudan/src/Main.java
+++ java-playground/gugudan/src/Main.java
@@ -6,6 +6,18 @@ public class Main {
     private static final int MIN = 2;
     private static final int MAX = 9;
 
+    private static int[] calculate(int number) {
+        int[] result = new int[MAX];
+        for (int i = 0; i < MAX; i++) {
+            result[i] = number * (i + 1);
+        }
+        return result;
+    }
+
+    private static void print(int[] dan) {
+        Arrays.stream(dan).forEach(System.out::println);
+    }
+
     public static void main(String[] args) {
         System.out.println("구구단 중 출력할 단은? : ");
         Scanner scanner = new Scanner(System.in);
@@ -16,11 +28,7 @@ public class Main {
             System.exit(1);
         }
 
-        int[] result = new int[MAX];
-        for (int i = 0; i < MAX; i++) {
-            result[i] = number * (i + 1);
-        }
-
-        Arrays.stream(result).forEach(System.out::println);
+        int[] dan = Main.calculate(number);
+        Main.print(dan);
     }
 }
