664f0c63c0680c1eab2273741d0c6f87f4303a5a
gurumee92 <gurumee92@woowahan.com>
2023-07-25 15:39:30 +0900

    code: playground #2 gugudan 추가 미션 #1

--- java-playground/gugudan/src/GuGudan.java
+++ java-playground/gugudan/src/GuGudan.java
@@ -2,27 +2,74 @@ import java.util.Arrays;
 
 public class GuGudan {
     public static final int MIN = 2;
-    public static final int MAX = 9;
+    public static final int MAX = 19;
     private final int factor;
-    private final int[] dan;
+    private int[] dan;
 
     public GuGudan(int factor) {
         this.factor = factor;
-        this.dan = new int[MAX];
-
-        for (int i = 0; i < MAX; i++) {
-            this.dan[i] = this.factor * (i + 1);
-        }
     }
     private boolean isValidate() {
         return MIN <= factor && factor <= MAX;
     }
 
-    public void print() throws Exception {
+    public void init() throws GugudanValidateException {
         if (!isValidate()) {
-            String message = String.format("factor %d는 %d-%d 사이의 값이 아닙니다.", factor, MIN, MAX);
-            throw new Exception(message);
+            throw new GugudanValidateException(factor);
         }
-        Arrays.stream(this.dan).forEach(System.out::println);
+
+        if (factor < 10) {
+            dan = new int[9];
+        } else {
+            dan = new int[MAX];
+        }
+
+        for (int i = 0; i < dan.length; i++) {
+            dan[i] = factor * (i + 1);
+        }
+    }
+
+    public boolean isInitialize() {
+        return dan == null;
+    }
+
+    public void print() throws GugudanInitializeException {
+        if (isInitialize()) {
+            throw new GugudanInitializeException();
+        }
+
+        System.out.printf("출력할 단은?: %d\n", factor);
+        for (int i = 0; i < dan.length; i++) {
+            System.out.printf("%d x %d = %d\n", factor, (i+1), dan[i]);
+        }
+    }
+
+    public void print(int finsihed) throws GugudanInitializeException {
+        if (isInitialize()) {
+            throw new GugudanInitializeException();
+        }
+
+        System.out.printf("출력할 단은?: %d\n", factor);
+        for (int i = 0; i < dan.length; i++) {
+            System.out.printf("%d x %d = %d\n", factor, (i+1), dan[i]);
+        }
+    }
+
+    private static class GugudanValidateException extends Exception{
+        private final int factor;
+        GugudanValidateException(int factor) {
+            this.factor = factor;
+        }
+
+        @Override
+        public String getMessage() {
+            return String.format("factor %d는 %d-%d 사이 수가 아닙니다.", factor, MIN, MAX);
+        }
+    }
+
+    private static class GugudanInitializeException extends Exception{
+        GugudanInitializeException() {
+            super("단이 초기화되지 않았습니다.");
+        }
     }
 }
--- java-playground/gugudan/src/Main.java
+++ java-playground/gugudan/src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        for (int i=1; i<=10; i++) {
-            System.out.printf("출력할 단: %d\n", i);
+        for (int i=1; i<=19; i++) {
             GuGudan dan = new GuGudan(i);
             try {
+                dan.init();
                 dan.print();
             } catch (Exception e) {
-                System.out.println(e.getMessage());
+                e.printStackTrace();
             }
         }
     }
