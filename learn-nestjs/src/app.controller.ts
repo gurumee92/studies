b83ce36da343d1d7cc184521695e2d8cd5747fc6
gurumee92 <gurumee92@woowahan.com>
2024-08-22 22:47:46 +0900

    cleanup

--- learn-nestjs/src/app.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get } from '@nestjs/common';
-
-@Controller('')
-export class AppController {
-  @Get()
-  home(): string {
-    return 'nestjs movies api';
-  }
-}
--- learn-nestjs/src/movies/movies.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { MoviesController } from './movies.controller';
-import { MoviesService } from './movies.service';
-
-@Module({
-  providers: [MoviesService],
-  controllers: [MoviesController],
-})
-export class MoviesModule {}
